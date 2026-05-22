9f57583fa66cfc40f6e55f5e3ba7119e1eceaa40
RicLps <armindoricardo656@gmail.com>
2026-05-21 23:26:26 -0100

    User comments storage

--- backend/entire-quizzes.php
+++ backend/entire-quizzes.php
@@ -5,14 +5,15 @@
         $selected_quiz_id = $_GET["quiz-id"];
         $quiz_data = new stdClass();
 
-        $quiz_sql = $connection->prepare("SELECT id, name, (SELECT CONCAT(first_name, ' ', last_name) FROM users WHERE quizzes.user_id = users.id) AS user_full_name FROM quizzes WHERE quizzes.id = ?;");
+        $quiz_sql = $connection->prepare("SELECT id, name, (SELECT CONCAT(first_name, ' ', last_name) FROM users WHERE quizzes.user_id = users.id) AS user_full_name, (SELECT id FROM users WHERE quizzes.user_id = users.id) AS user_id FROM quizzes WHERE quizzes.id = ?;");
         $quiz_sql->bind_param("s", $selected_quiz_id);
         $quiz_sql->execute();
         $result = $quiz_sql->get_result();
         $quiz = $result->fetch_assoc();
         
         $quiz_data->name = $quiz["name"];
-        $quiz_data->user = $quiz["user_full_name"];
+        $quiz_data->user_name = $quiz["user_full_name"];
+        $quiz_data->user_id = $quiz["user_id"];
 
         $questions_sql = $connection->prepare("SELECT id, type, text FROM questions WHERE questions.quiz_id = ?;");
         $questions_sql->bind_param("s", $quiz["id"]);
@@ -51,6 +52,16 @@
 
         $sql = $connection->prepare("UPDATE quizzes SET submit_count = submit_count + 1 WHERE id = ?;");
         $sql->bind_param("s", $quiz_id);
+        $sql->execute();
+    }
+
+    if(isset($_POST["user-comment"])){
+        $comment = $_POST["user-comment"];
+        $user_id = $_POST["user-id"];
+        $comment_quiz_id = $_POST["quiz-id"];
+
+        $sql = $connection->prepare("INSERT INTO comments (user_id, quiz_id, text) VALUES (?, ?, ?);");
+        $sql->bind_param("sss", $user_id, $comment_quiz_id, $comment);
         $sql->execute();
     }
 ?>
