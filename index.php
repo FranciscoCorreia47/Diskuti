613a5417614baecca058a4bef9cc89d5738dfbd0
RicLps <armindoricardo656@gmail.com>
2026-06-18 17:28:20 -0100

    Added new UI elements

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
   <script src="https://js.pusher.com/8.0.1/pusher.min.js"></script>
   <title>Diskuti | Home</title>
 </head>
-<body>
+<body class="body">
   <main>
     <section class="menu vertical">
       <div>
